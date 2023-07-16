984b3c0d1c9e1050d6587f33bb41c8e6b0878b67
zaidy <zaidyasser022@gamil.com>
2023-07-16 12:54:25 +0300

    25

---
+++ .android/app/src/main/java/com/example/dio_http_cache/host/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.dio_http_cache.host;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- .ios/Flutter/flutter_export_environment.sh
+++ .ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\zaidy\flutter"
-export "FLUTTER_APPLICATION_PATH=A:\flutter-p\letsia-p\المحميه\dio-http-cache"
+export "FLUTTER_APPLICATION_PATH=A:\flutter-p\New folder\dio-cache-update"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
